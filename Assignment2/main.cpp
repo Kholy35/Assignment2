b6fc5221eccbb5b119243d945002e960b32af1c5
“Essam <essamh@aucegypt.edu>
2023-03-05 19:34:22 +0200

    Initial Commit

---
+++ Assignment2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Assignment2
+//
+//  Created by Essam Elkholy on 05/03/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
